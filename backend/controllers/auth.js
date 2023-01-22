01de28323095532e8d0cd020e24ad655ce984bf6
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-01-22 21:57:44 +0100

    Init

---
+++ backend/controllers/auth.js
@@ -0,0 +1,84 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* Login */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User doesn't exist" });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      res.status(400).json({ message: "Invalid Credentails" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    console.log('here: ',error)
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* get User */
+
+export const getUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* getUserFriends */
+export const getUserFriends = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* addRemoveFriend */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
