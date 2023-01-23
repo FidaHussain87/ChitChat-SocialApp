672ae2514056d39c3d144f891e1ff9362b9192e2
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-01-23 01:52:47 +0100

    babel added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
 export const register = async (req, res) => {
   try {
     const {
